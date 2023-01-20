075f435d53d74c8d0613d9254e413f3996d23106
Анастасия <iamhrusha@gmail.com>
2023-01-21 01:37:11 +0300

    CityServiceTest

--- src/test/java/org/anastasia/citiesinfoapplication/dao/CityServiceTest.java
+++ src/test/java/org/anastasia/citiesinfoapplication/dao/CityServiceTest.java
@@ -1,6 +1,7 @@
 package org.anastasia.citiesinfoapplication.dao;
 
 import org.anastasia.citiesinfoapplication.model.City;
+import org.anastasia.citiesinfoapplication.service.CityService;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -8,11 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class CityDaoTest {
+public class CityServiceTest {
+    private final CityService cityService;
     private final CityDao cityDao;
 
     @Autowired
-    public CityDaoTest(CityDao cityDao) {
+    public CityServiceTest(CityService cityService, CityDao cityDao) {
+        this.cityService = cityService;
         this.cityDao = cityDao;
     }
 
@@ -23,36 +26,36 @@ public class CityDaoTest {
 
     @Test
     void saveAndFindByIdTest() {
-        City cityForSave = new City("Якутск", 14);
-        cityDao.save(cityForSave);
-        City foundCityById = cityDao.findById(cityForSave.getId()).get();
-        Assertions.assertEquals(cityForSave, foundCityById);
+        City city = new City("Калининград", 39);
+        cityDao.save(city);
+        City foundCityById = cityDao.findById(city.getId()).get();
+        Assertions.assertEquals(city, foundCityById);
     }
 
     @Test
-    void findCityByRegionNumberTest() {
+    void findByRegionNumberTest() {
         City city = new City("Калининград", 39);
         cityDao.save(city);
-        City foundCity = cityDao.findByRegionNumber(city.getRegionNumber()).get();
-        Assertions.assertEquals(city, foundCity);
+        City foundCityByRegionNumber = cityDao.findByRegionNumber(city.getRegionNumber()).get();
+        Assertions.assertEquals(city, foundCityByRegionNumber);
     }
 
     @Test
     void findAllTest() {
-        cityDao.save(new City("Ярославль", 76));
+        cityDao.save(new City("Калининград", 39));
         cityDao.save(new City("Краснодар", 23));
         Assertions.assertEquals(2, cityDao.findAll().size());
     }
 
     @Test
     void deleteByIdTest() {
-        City city = new City(10L, "Калининград", 39);
+        City city = new City(1L, "Краснодар", 23);
         cityDao.deleteById(city.getId());
         Assertions.assertEquals(0, cityDao.findAll().size());
     }
 
     @Test
-    void deleteByRegionNumberTest() {
+    void deleteByRegionNumber() {
         City city = new City("Калининград", 39);
         cityDao.deleteByRegionNumber(city.getRegionNumber());
         Assertions.assertEquals(0, cityDao.findAll().size());
